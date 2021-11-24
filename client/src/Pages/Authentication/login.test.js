3193e8c13a25b455f133407955fdcbef9b7e779f
mohammed <bhanpurawala@capitalnumbers.com>
2021-11-24 18:14:23 +0530

    Add Test Case for login

--- client/src/Pages/Authentication/login.test.js
+++ client/src/Pages/Authentication/login.test.js
@@ -1,15 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import { shallow } from "enzyme";
+// import { render, screen } from "@testing-library/react";
+import { shallow,configure } from "enzyme";
+import Adapter from 'enzyme-adapter-react-17-updated';
 import Login from "./login";
 
-// test("Render Login Page", () => {
-//   render(<Login />);
-//   expect(screen.getByTestId("login")).toBeEnabled();
-// });
-
-let wrapper;
-
-test("email check", () => {
-  render(<Login />);
-  expect(screen.getByPlaceholderText("Email")).simulate("change",{ value: "admin@admin.com" });
+configure({adapter: new Adapter()});
+describe("Test case for testing login", () => {
+  let wrapper;
+  test("email check", () => {
+    wrapper = shallow(<Login />);
+    wrapper
+      .find('input[type="email"]')
+      .simulate("change", {
+        target: { name: "email", value: "admin@admin.com" },
+      });
+    expect(wrapper.state("email")).toEqual("admin@admin.com");
+  });
+  it("password check", () => {
+    wrapper = shallow(<Login />);
+    wrapper
+      .find('input[type="password"]')
+      .simulate("change", {
+        target: { name: "password", value: "123456" },
+      });
+    expect(wrapper.state("password")).toEqual("123456");
+  });
 });
