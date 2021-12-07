27d67c5da44955c24a18d039f3a81a3a7dc4c831
Shubham Aggarwal <shubhamaggarwal@Shubhams-MacBook-Air.local>
2021-12-07 18:28:55 +0530

    added redis support

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 const { Category } = require("../models/category");
+const { createClient } = require('redis');
 
 // Getting all Category Data
-exports.categories_all = (req, res) => {
+exports.categories_all = async (req, res) => {
+  //change host to localhost and port accordingly
+  const client = createClient({host: '0.0.0.0', port: 6379});
+  await client.connect();
+  const value = await client.get('key')
+  console.log(value)
   Category.find({}, (err, data) => {
     if (!err) {
       res.status(200).json(data);
