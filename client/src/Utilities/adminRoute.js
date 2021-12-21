068badd6c356a66e2332dcd206aa167b9d27ae72
mohammed <bhanpurawala@capitalnumbers.com>
2021-12-21 18:11:28 +0530

    make admin route

--- client/src/Utilities/adminRoute.js
+++ client/src/Utilities/adminRoute.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./authContext";
+// import { Button, Icon } from "semantic-ui-react";
+function AdminChild({ children, adminUser }) {
+  return adminUser ? children : <Navigate to="/" />;
+}
 
 function AdminRoute({ children }) {
   const { currentUser } = useAuth();
   const [adminUser, setAdminUser] = useState(false);
   useEffect(() => {
-    currentUser.getIdTokenResult().then((idTokenResult) => {
-      if (!!idTokenResult.claims.admin) {
-        setAdminUser(idTokenResult.claims.admin);
-        console.log(idTokenResult.claims.admin);
-      }
-    });
+    if (currentUser) {
+      currentUser.getIdTokenResult().then((idTokenResult) => {
+        if (!!idTokenResult.claims.admin) {
+          setAdminUser(idTokenResult.claims.admin);
+        }
+      });
+    }
   });
 
-  return adminUser ? children : <Navigate to="/" />;
+  return (
+    <div>
+      {adminUser && <AdminChild adminUser={adminUser} children={children} />}
+    </div>
+  );
 }
 
 export default AdminRoute;
